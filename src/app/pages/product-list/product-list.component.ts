f055ae83a571fad0957b496fb2ee96b94f2a8aa2
Fatrai1 <fatrai.cb@gmail.com>
2021-03-18 10:42:08 +0100

    delete gomb

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
+import { Observable} from 'rxjs';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  productList$: Observable<Product[]> = this.productService.productList$;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.productService.getAll();
   }
 
+  deleteItem(product: Product): void {
+    this.productService.remove(product).subscribe(
+      () => location.reload()
+    );
+  }
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -11,5 +11,17 @@ import { Product } from 'src/app/model/product';
 })
 export class ProductService {
 
+  apiUrl: string = "http://localhost:3000/products";
+  productList$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
   constructor(private http: HttpClient) { }
+
+  getAll(): void {
+    this.http.get<Product[]>(this.apiUrl).subscribe(
+      products => this.productList$.next(products)
+    );
+  }
+  remove(product: Product): Observable<Product[]> {
+    return this.http.delete<Product[]>(`${this.apiUrl}/${product.id}`);
+  }
 }
